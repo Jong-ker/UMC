abeb051ffe126beeffe3192251ad1c6c24e89cd0
Jongkwon <99729835+Jong-ker@users.noreply.github.com>
2023-10-15 16:27:55 +0900

    Add files via upload

---
+++ market/FeedTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  FeedTableViewCell.swift
+//  market
+//
+//  Created by Jongkwon on 10/15/23.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageViewPicture: UIImageView!
+    
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var labelPlace: UILabel!
+    
+    @IBOutlet weak var labelPrice: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        imageViewPicture.layer.cornerRadius = 22.5
+        imageViewPicture.clipsToBounds = true
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ market/market/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  market
+//
+//  Created by Jongkwon on 10/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
